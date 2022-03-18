3147986ace6f7ffb4b0207ff2a42cd08a9add185
Hai <lhhai.1208@gmail.com>
2022-03-17 23:15:21 -0500

    implemented clean storage functionalities

--- converter/models.py
+++ converter/models.py
@@ -2,4 +2,3 @@ from django.db import models
 
 class File(models.Model):
     filename = models.CharField(max_length=500)
-    file_url = models.FileField()
--- converter/views.py
+++ converter/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from django.http import HttpResponse, JsonResponse
 import pandas as pd
 import os
-from datetime import datetime
+from datetime import datetime as dtime
 import re
 from django.conf import settings
 from rest_framework import status, serializers
@@ -13,6 +13,7 @@ from converter.models import File
 
 from datetime import timedelta
 from pathlib import Path
+import datetime
 
 import boto3
 import io
@@ -21,34 +22,36 @@ import io
 def should_delete(filename):
     path = Path(filename).stem
     dt_str = path[-20:-10]
-    target_dt = datetime.datetime.today() - timedelta(hours = 2) # check for all files created 2 hours ago
+    target_dt = datetime.datetime.today() - timedelta(hours = 1) # check for all files created 2 hours ago
     dt = datetime.datetime(int(dt_str[:4]), int(dt_str[4:6]), int(dt_str[6:8]), int(dt_str[8:]))
     return dt < target_dt
 
+
 @api_view(['POST',])
 def clean_storage(request):
-    files = os.listdir("./media") # find all files in the media folder
+    files = File.objects.all() # find all files in the media folder
     if len(files) == 0: return JsonResponse({"msg": "Storage is clear"}, status=status.HTTP_200_OK)
 
+    filenames = [str(f.filename) for f in files]
+    if len(filenames) == 0: return JsonResponse({"msg": "No files to delete"}, status=status.HTTP_200_OK)
     deleted_files = list() # store the results
+
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
+    )
+
     try:
-        for path in files:
+        for path in filenames:
             if should_delete(path):
                 deleted_files.append(path)
-                os.remove(os.path.join("media", path))
+                s3_client.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=path)
     except Exception as e:
         return JsonResponse({"msg": str(e)}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
     return JsonResponse({"msg": "Deleting done", "deleted_files": deleted_files}, status=status.HTTP_200_OK)
 
-@api_view(['GET',])
-def download_file(request, filename):
-    filepath = os.path.join(settings.BASE_DIR, "media", filename)
-    path = open(filepath, 'rb')
-    mime_type, _ = mimetypes.guess_type(filepath)
-    response = HttpResponse(path, content_type = mime_type)
-    response['Content-Disposition'] = "attachment; filename=%s" % filename
-    return response
 
 @api_view(["POST",])
 def convert_csv(request, output_format):
@@ -73,32 +76,32 @@ def convert_csv(request, output_format):
 
         # for html output
         if output_format == "html":
-            filename = f.name[:-4] + "_" + "".join(re.findall('[0-9]+', str(datetime.now()))) + ".html"
+            filename = f.name[:-4] + "_" + "".join(re.findall('[0-9]+', str(dtime.now()))) + ".html"
             filepath = os.path.join("media/", filename)
             data.to_html(filepath)
 
         # output to excel file
         elif output_format == "xlsx":
-            filename = f.name[:-4] + "_" + "".join(re.findall('[0-9]+', str(datetime.now()))) + ".xlsx"
+            filename = f.name[:-4] + "_" + "".join(re.findall('[0-9]+', str(dtime.now()))) + ".xlsx"
             filepath = os.path.join("media/", filename)
             print(filepath)
             data.to_excel(filepath)
 
         # output to json file
         elif output_format == "json":
-            filename = f.name[:-4] + "_" + "".join(re.findall('[0-9]+', str(datetime.now()))) + ".json"
+            filename = f.name[:-4] + "_" + "".join(re.findall('[0-9]+', str(dtime.now()))) + ".json"
             filepath = os.path.join("media/", filename)
             data.to_json(filepath)
 
         # output to text file
         elif output_format == "txt":
-            filename = f.name[:-4] + "_" + "".join(re.findall('[0-9]+', str(datetime.now()))) + ".txt"
+            filename = f.name[:-4] + "_" + "".join(re.findall('[0-9]+', str(dtime.now()))) + ".txt"
             filepath = os.path.join("media/", filename)
             data.to_csv(filepath)
 
         # output to pickle file
         elif output_format == "pkl":
-            filename = f.name[:-4] + "_" + "".join(re.findall('[0-9]+', str(datetime.now()))) + ".pkl"
+            filename = f.name[:-4] + "_" + "".join(re.findall('[0-9]+', str(dtime.now()))) + ".pkl"
             filepath = os.path.join("media/", filename)
             data.to_pickle(filepath)
 
@@ -114,7 +117,7 @@ def convert_csv(request, output_format):
             for column_name in data.columns:
                 data.rename(columns = {str(column_name) : str(re.sub('[^A-Za-z0-9_ ]+', '', str(column_name))).replace(" ", "_")}, inplace = True)
 
-            filename = f.name[:-4] + "_" + "".join(re.findall('[0-9]+', str(datetime.now()))) + ".xml"
+            filename = f.name[:-4] + "_" + "".join(re.findall('[0-9]+', str(dtime.now()))) + ".xml"
             filepath = os.path.join("media/", filename)
             
             data.to_xml(filepath)
@@ -122,6 +125,9 @@ def convert_csv(request, output_format):
         response = s3_client.put_object(
             Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=filename, Body=open(filepath, "rb")
         )
+
+        new_file = File(filename=filename)
+        new_file.save()
 
         try:
             os.remove(filepath)
